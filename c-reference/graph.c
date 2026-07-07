4acf72d19d0d2d5570f7935ea82535c31a9caf9b
Matthew Zollo <mzollo91@gmail.com>
2026-07-07 11:13:30 -0400

    Completed free_graph(), working on add_node.

--- c-reference/graph.c
+++ c-reference/graph.c
@@ -24,3 +24,23 @@ Graph *create_graph(size_t capacity)
 
     return g;
 }
+
+void free_graph(Graph *g)
+{
+    // Use nodeCount for the loop as this is incremented for each node
+    for (int i = 0; i < g->nodeCount; i++)
+    {
+        Node *n = &g->nodeArr[i]; // Use the '&' symbol here to point to the memory address of the node in the array.
+        Edge *current = n->head;
+        while (current != NULL)
+        {
+            Edge *temp = current->next;
+            free(current);
+            current = temp;
+        }
+    }
+    // since the memory is allocated for the entire array,
+    // it must be freed the same way rather than by individual node.
+    free(g->nodeArr);
+    free(g);
+}
