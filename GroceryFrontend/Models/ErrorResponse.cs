730778cd7927ba7653a8d97317e1a1ad73a70707
Matthew Zollo <mzollo91@gmail.com>
2026-08-02 17:34:05 -0400

    CalculateRoute progressing. General function structure is laid out, cleanup needed.

---
+++ GroceryFrontend/Models/ErrorResponse.cs
@@ -0,0 +1,9 @@
+public class ListErrorResponse
+{
+    public List<int> Detail { get; set; } = new();
+}
+
+public class DictErrorResponse
+{
+    public Dictionary<string, int> Detail { get; set; } = new();
+}
