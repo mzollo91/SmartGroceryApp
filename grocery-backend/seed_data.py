6a5f6cb2f0b93cf8d0f90fc6834bd57115fcc6c3
Matthew Zollo <mzollo91@gmail.com>
2026-08-03 16:57:23 -0400

    HTTP response testing complete, test  page works as expected.

--- grocery-backend/seed_data.py
+++ grocery-backend/seed_data.py
@@ -5,7 +5,7 @@ from models import Store, Aisle, Edge
 
 async def seed():
     async with AsyncSessionLocal() as session:
-        store = Store(name="Test Store")
+        store = Store(name="Giant")
         session.add(store)
         await session.flush() # Complete transaction without ending session.
 
@@ -20,6 +20,24 @@ async def seed():
             Edge(aisle_a_id=entrance.id, aisle_b_id=produce.id, distance=5.0),
             Edge(aisle_a_id=produce.id, aisle_b_id=dairy.id, distance=3.0),
             Edge(aisle_a_id=dairy.id, aisle_b_id=checkout.id, distance=4.0),
+        ]
+        session.add_all(edges)
+
+        store = Store(name="Walmart")
+        session.add(store)
+        await session.flush() # Complete transaction without ending session.
+
+        entrance = Aisle(name="Entrance", store_id=store.id)
+        produce = Aisle(name="Produce", store_id=store.id)
+        dairy = Aisle(name="Dairy", store_id=store.id)
+        checkout = Aisle(name="Checkout", store_id=store.id)
+        session.add_all([entrance, produce, dairy, checkout])
+        await session.flush()  # assigns ids to each aisle
+        
+        edges = [
+            Edge(aisle_a_id=entrance.id, aisle_b_id=produce.id, distance=7.0),
+            Edge(aisle_a_id=produce.id, aisle_b_id=dairy.id, distance=10.0),
+            Edge(aisle_a_id=dairy.id, aisle_b_id=checkout.id, distance=3.0),
         ]
         session.add_all(edges)
 
